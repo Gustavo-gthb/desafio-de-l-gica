8beb5feacaa9c2dc9cdeb4f6726e5582e789a0c1
Gustavo-dev <playerforever81@gmail.com>
2024-09-28 22:58:32 -0300

    fix: challenge 7

---
+++ desafio7.py
@@ -0,0 +1,6 @@
+# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+valor_do_lado_do_quadrado = int(input('digite o valor do lado do quadrado: '))
+area_do_quadrado = valor_do_lado_do_quadrado ** 2
+dobro_da_area = area_do_quadrado * 2
+print('o valor da area do quadrado é', dobro_da_area)
