2c45034087c2a36a37f461c64a9a62731ea77e16
Gustavo-dev <playerforever81@gmail.com>
2024-09-29 00:22:12 -0300

    fix: challenge 13

--- desafio13.py
+++ desafio13.py
@@ -1,7 +1,7 @@
 # Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas: Para homens: (72.7h) - 58 Para mulheres: (62.1h) - 44.7
 
 altura = float(input('Digite a sua altura: '))
-sexo = input('qual é o seu sexo? (m/f)')
+sexo = str.lower(input('qual é o seu sexo? (m/f)'))
 if sexo == 'm':
     masculino = (72.7 * altura) - 58
     print(masculino)
