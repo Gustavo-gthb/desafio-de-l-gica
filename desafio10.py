30cca5bf8688994a1b48132c023ba233370ddc37
Gustavo-dev <playerforever81@gmail.com>
2024-09-28 23:14:56 -0300

    fix: challenge 10

---
+++ desafio10.py
@@ -0,0 +1,5 @@
+# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Farenheit.
+
+temperatura_celsius = float(input('Valor em celsius: '))
+conversao_farenheit = temperatura_celsius * 1.8 + 32
+print('valor em farenheit: ', conversao_farenheit)
