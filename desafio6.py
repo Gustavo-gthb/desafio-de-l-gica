f85360b0079a896edecd81c7da0e4fda323e15c6
Gustavo-dev <playerforever81@gmail.com>
2024-09-28 21:42:33 -0300

    fix: challenge 6

--- desafio6.py
+++ desafio6.py
@@ -1,2 +1,7 @@
 # Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
 
+import math
+
+raio_do_circulo = int(input('Digite o raio de um círculo: '))
+area = math.pi * raio_do_circulo ** 2
+print('area do círculo é: ', area)
