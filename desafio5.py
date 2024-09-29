f3c66fb59b0aa927f6f6453e4289a60496216ca7
Gustavo-dev <playerforever81@gmail.com>
2024-09-28 21:30:26 -0300

    fix: challenge 5

---
+++ desafio5.py
@@ -0,0 +1,5 @@
+# Faça um Programa que converta metros para centímetros.
+
+valor_em_metros = int(input('Digite o valor em metros: '))
+conversao = valor_em_metros * 100
+print('o valor em centímetros é: ' + str(conversao))
